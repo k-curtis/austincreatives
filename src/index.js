de5a643d84cc4e8239e689c5710ffd6b79e41fcf
Kevin Curtis <kevin@k-curtis.com>
2021-02-19 13:33:44 -0600

    fixed material ui import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import { ThemeProvider, createMuiTheme } from "@material-ui";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core";
 
 const theme = createMuiTheme({
   typography: {
